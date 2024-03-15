b8c8e2b0355c431fc161d39086c9fafd2f3d0265
hwnagminwoo <135596448+hwangminwoo4@users.noreply.github.com>
2024-03-15 12:21:01 +0900

    add : JDBCTemplate 클래스 추가
    add : JDBCTemplate 클래스 추가
    author : 황민우
    
    src\main\java\com\galgoda\common\template\JDBCTemplate.java

---
+++ src/main/java/com/galgoda/common/template/JDBCTemplate.java
@@ -0,0 +1,93 @@
+package com.galgoda.common.template;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		Properties prop = null;
+		
+		String filePath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+		
+		try {
+			prop.load(new FileInputStream(filePath));
+			Class.forName(prop.getProperty("driver"));
+			
+			conn = DriverManager.getConnection(prop.getProperty("url")
+					                         , prop.getProperty("username")
+					                         , prop.getProperty("password"));
+			
+			conn.setAutoCommit(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	// 커밋 메소드
+	public static void commit(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 롤백 메소드
+	public static void rollback(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Connection객체 반납 메소드
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Statement객체 반납 메소드
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// ResultSet객체 반납 메소드
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
