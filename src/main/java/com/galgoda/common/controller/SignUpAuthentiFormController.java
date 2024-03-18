2a9eea62b322486ca45732b760d74abad414b565
MIRO <regina2504@naver.com>
2024-03-18 11:57:33 +0900

    add : common 폴더 회원가입 메인, 본인인증 관련 controller 추가
    add : common 폴더 회원가입 메인, 본인인증 관련 controller 추가
    author : 이다예
    
    - SignUpAuthenti
    - SignUpAuthentiForm
    - SignUpChoice
    - SignUpMain

--- src/main/java/com/galgoda/common/controller/SignUpAuthentiFormController.java
+++ src/main/java/com/galgoda/common/controller/SignUpAuthentiFormController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SignUpController
+ * Servlet implementation class SignUpAuthentiFormController
  */
-@WebServlet("/signupMain.co")
-public class SignUpMainController extends HttpServlet {
+@WebServlet("/authenti.co")
+public class SignUpAuthentiFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SignUpMainController() {
+    public SignUpAuthentiFormController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,8 +27,7 @@ public class SignUpMainController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.getRequestDispatcher("/views/common/signupTerms.jsp").forward(request, response);
-		
+		request.getRequestDispatcher("/views/common/authenti.jsp").forward(request, response);
 	}
 
 	/**
