84c4110cee95f9c94ddb017ba32fb0981b18a4f8
llsh93 <153481479+llsh93@users.noreply.github.com>
2024-03-19 12:16:33 +0900

    modify : 파일명 변경
    modify : 파일명 변경
    author : 이상현
    
    modify : 파일명 controller 빠진거 수정

--- src/main/java/com/galgoda/supervisor/controller/SupervisorDetailTermsController.java
+++ src/main/java/com/galgoda/supervisor/controller/SupervisorDetailTermsController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class SupervisorEnrollTermForm
+ * Servlet implementation class SupervisorDetailTerms
  */
-@WebServlet("/enrollTermForm.su")
-public class SupervisorEnrollTermForm extends HttpServlet {
+@WebServlet("/detailTerms.su")
+public class SupervisorDetailTermsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SupervisorEnrollTermForm() {
+    public SupervisorDetailTermsController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,7 +26,7 @@ public class SupervisorEnrollTermForm extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/supervisor/supervisorEnrollTerm.jsp").forward(request, response);
+		
 	}
 
 	/**
