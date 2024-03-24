14b95d095d54c9c99b525bc1ba026fa5f9385889
MIRO <regina2504@naver.com>
2024-03-24 13:12:40 +0900

    add : common 폴더 호텔 회원가입 시 이메일/아이디중복체크 controller 추가
    add : common 폴더 호텔 회원가입 시 이메일/아이디중복체크 controller 추가
    author : 이다예
    - 호텔회원가입 시 이메일/아이디 중복체크 별도로 controller 추가

---
+++ src/main/java/com/galgoda/common/controller/SignUpHotelEmailCheckController.java
@@ -0,0 +1,57 @@
+package com.galgoda.common.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import com.galgoda.common.model.service.CommonService;
+
+/**
+ * Servlet implementation class SignUpHotelEmailCheckController
+ */
+@WebServlet("/hotelEmailDoubleCheck.co")
+public class SignUpHotelEmailCheckController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUpHotelEmailCheckController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 클라이언트로부터 받은 이메일 파라미터 가져오기
+        String email = request.getParameter("email");
+
+        // 이메일 중복 확인 서비스 호출
+        boolean isEmailDuplicate = new CommonService().isHotelEmailDuplicate(email);
+
+        // JSON 형식으로 결과 전달
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("isEmailDuplicate", isEmailDuplicate);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonResponse.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
